ce518082d1f2d1a54d2e50aecafd24998f20d197
reptilefury <reptilefury@outlook.com>
2021-07-28 05:15:28 +0100

    Arrays

---
+++ src/main/kotlin/Chapter 8 Arrays and Lists/Arrays.kt
@@ -0,0 +1,22 @@
+package `Chapter 8 Arrays and Lists`
+
+//arrays should contain only one type of value i.e don't mix strings and Integers.
+fun main(args: Array<String>) {
+    //use the function: arrayOf() to create an array.
+    val evenNumbers = arrayOf(2, 4, 6, 8)
+    //the array above Kotlin has inferred it to be of a type Int, this type is written as Array<Int>
+    // you can also create an array with all of its values set to default like:
+    val fiveFives = Array(5,{5}) //5,5,5,5,5,5
+    //declare arrays that are not going to change as constants using the val keyword.
+    val vowels  = arrayOf("a", "e", "i", "o", "u" )
+    //below is an array of odd numbers, there are more kotlin functions for dealing with arrays.
+
+    val oddNumbers = intArrayOf(1,3,5,7)
+    // the other standard library functions include floatArrayOf(), doubleArrayOf() and booleanArrayOf()
+    val zeros = DoubleArray(4) //the number indicates the size of the array
+    //You can also convert between the boxed and primitive arrays using functions such as:
+    val otherOddNumbers = arrayOf(1,3,5,7).toIntArray()
+
+
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,39 +1,5 @@
-/*
-import sun.rmi.runtime.Log
-
-fun main(args: Array<String>) {
-    class Person(val name: String, var weightLbs: Double) {
-        fun eatDessert(addedIcream: Boolean = true) {
-         weightLbs += if (addedIcream) 4.0 else 2.0
-        }
-        fun calcGoalWeight(weightToLoose: Double): Double {
-           return weightLbs - weightToLoose
-        }
-        val p = Person("John Doe", 170.0)
-        p.eatDessert(true)
-        val lbs = p.weightLbs
-        p.eatDessert()
-        lbs = p.weightLbs
-        val gw = p.calcGoalWeight()
-
-
-    }
-}
-*/
 fun main(args:Array<String>){
-    var map = HashMap<Int, String>()
-    map.put(213,"Jenna")
-    map.put(41,"Lena")
-    map.put(32,"Seehaus")
-    map.put(46,"Farz")
 
-    println(map.get(213))
-    println(map.get(41))
-    println(map.get(32))
-    println(map.get(46))
-    for(k in map.keys){
-        println(map.get(k))
-    }
 
 
 }
