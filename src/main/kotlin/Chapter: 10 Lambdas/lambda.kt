306139cccd8736d758afe05bea10c93d55e7309e
reptilefury <reptilefury@outlook.com>
2021-08-08 16:42:22 +0100

    added Unit Lambda

--- src/main/kotlin/Chapter: 10 Lambdas/lambda.kt
+++ src/main/kotlin/Chapter: 10 Lambdas/lambda.kt
@@ -1,20 +1,31 @@
-fun main(args: Array<String>){
- var multiplyLambda: (Int, Int) -> Int
- multiplyLambda = {
-     a: Int,b: Int -> Int
-     a * b
- }
-    val lambdaResult = multiplyLambda(4,2)
+fun main(args: Array<String>) {
+    var multiplyLambda: (Int, Int) -> Int
+    multiplyLambda = { a: Int, b: Int ->
+        Int
+        a * b
+    }
+    val lambdaResult = multiplyLambda(4, 2)
 
     //using lambdas as arguments
-    fun operateOnNumbers(a:Int, b:Int, operation: (Int,Int) -> Int):Int{
-        val result = operation(a,b)
+    fun operateOnNumbers(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
+        val result = operation(a, b)
         println(result)
         return result
-      val addLambda = {a: Int, b: Int ->
-          a + b
-      }
+        val addLambda = { a: Int, b: Int ->
+            a + b
+        }
         operateOnNumbers(4, 2, addLambda)
     }
 
+    //there is no need to define a lambda you can declare it this way
+
+operateOnNumbers(4,2, operation = Int::plus)
+    operateOnNumbers(4,2) {a,b ->
+
+        a + b
+    }
+    var unitLambda:() -> Unit ={
+        println("Kotline Apprentice is awesome!")
+    }
+    unitLambda()
 }
