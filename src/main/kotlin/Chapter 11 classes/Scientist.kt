23e2dc89e684d93c821fe5e880e78a6266c40469
reptilefury <reptilefury@outlook.com>
2021-08-19 04:32:20 +0100

    completed an exercise

--- src/main/kotlin/Chapter 11 classes/Scientist.kt
+++ src/main/kotlin/Chapter 11 classes/Scientist.kt
@@ -43,9 +43,23 @@ fun main(args: Array<String>){
     ScientistRepository.addScientist(Jenna)
     ScientistRepository.addScientist(Lena)
     ScientistRepository.addScientist(Isla)
+    val Isla = Student2(1, "Isla", "Muller")
+    val Stephen = Student2(2, "Stephen", "Hawkings")
+    println(Student2.numberOfStudents())
 
 }
 var Anna = Scientist.newScientist("Anna", "Seehaus")
 var Jenna = Scientist.newScientist("Jenna", "Cottrel")
 var Lena = Scientist.newScientist("Lena", "Bs")
 var Isla = Scientist.newScientist("Isla", "Muller")
+data class Student2(val id: Int,val firstName: String,val lastName: String){
+    var fullName = "$firstName $lastName"
+    companion object{
+        var counter = 0
+        fun numberOfStudents()= counter
+
+    }
+    init {
+        counter ++
+    }
+}
