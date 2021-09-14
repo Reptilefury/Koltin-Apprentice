03aad714fcf135abb4dd8e47130623a5063240bc
reptilefury <reptilefury@outlook.com>
2021-09-14 06:33:46 +0100

    worked on sealed classes

--- src/main/kotlin/Objects/sealed classes.kt
+++ src/main/kotlin/Objects/sealed classes.kt
@@ -1,22 +1,31 @@
 package Objects
 
 sealed class Shape{
+    class Circle(val radius: Int) : Shape()
+    class Square(val sideLength: Int) : Shape()
 
-    class Circle(val radius:Int):Shape()
-    class Square(val sideLength:Int):Shape()
+    val Circle1 = Circle(4)
+    val Circle2 = Circle(6)
+    val Square1 = Square(8)
+    val Square2 = Square(6)
 
-    val Circle1 = Circle(5)
-    val Circle2 = Circle(4)
-    val Square1 = Square(6)
-    val Square2 = Square(4)
-
-    fun Size(shape:Shape):Int {
-        return when(shape){
+    fun Size(shape: Shape) {
+        when (shape) {
             is Shape.Circle -> shape.radius
             is Shape.Square -> shape.sideLength
-
         }
-
     }
+}
+sealed class Shape1{
+    object none: Shape1()
+    data class Circle(val radius: Int):Shape1()
+    data class Rectangle(val width: Int, val height:Int):Shape1()
 
+}
+fun main(){
+     fun AreaOf(shape: Shape1):Number = when (shape){
+         is Shape1.none  -> 2
+         is Shape1.Circle -> 4
+         is Shape1.Rectangle -> 6
+     }
 }
