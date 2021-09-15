b77f1f9e74bb39a38e1b8198bc5ba42d1d6e43af
reptilefury <reptilefury@outlook.com>
2021-09-15 04:41:24 +0100

    Still working on sealed classes

--- src/main/kotlin/Objects/sealed classes.kt
+++ src/main/kotlin/Objects/sealed classes.kt
@@ -24,18 +24,53 @@ sealed class Shape1{
 }
 
 sealed class Data{
+    class Success(val success:String):Data()
     class Error(val error:String):Data()
-    class Success(val success: String): Data()
-     object Loading
-   // class Loading(val loading: String):Data()
+    class Loading(val loading: String):Data()
 
 }
-fun getData():Data{
-
+fun  getData():Data{
     return Data.Success((100..1000).random().toString())
 }
+/*sealed class Data{
+    class Error(val error:String):Data()
+    class Success(val success: String): Data()
+    class Loading(val loading:String): Data()
+     //object Loading
+   // class Loading(val loading: String):Data()
+
+}*/
+/*fun getData():Data{
+
+    return Data.Success((100..1000).random().toString())
+}*/
 fun main(){
-     fun AreaOf(shape: Shape1):Number = when (shape){
+    val data = getData()
+    when(data){
+        is Data.Success -> {
+            println(data)
+        }
+        is Data.Error -> {
+            println(data)
+        }
+        is Data.Loading ->{
+            println(data)
+        }
+    }
+
+/*    val data = getData()
+    when(data){
+        is Data.Success -> {
+            println(data)
+        }
+        is Data.Error -> {
+            println(data)
+        }
+         is Data.Loading -> {
+             println(data)
+         }
+    }*/
+    fun AreaOf(shape: Shape1):Number = when (shape){
          is Shape1.none  -> 2
          is Shape1.Circle -> 4
          is Shape1.Rectangle -> 6
