8a8f199f420f72bdec5ed3886e32548a523459b2
reptilefury <reptilefury@outlook.com>
2021-09-30 03:31:46 +0100

    Will start implementing multiple interfaces

--- src/main/kotlin/interface/vehicle.kt
+++ src/main/kotlin/interface/vehicle.kt
@@ -1,12 +1,16 @@
 package `interface`
 
+import jdk.incubator.vector.DoubleVector
+import kotlin.math.PI
+
 interface vehicle {
     fun Accelerating()
     fun Stop()
 }
-class Unicycle:vehicle{
+
+class Unicycle : vehicle {
     var peddling = false
-    override fun Accelerating(){
+    override fun Accelerating() {
         peddling = true
     }
     override fun Stop(){
@@ -93,8 +97,9 @@ class LightFreighter: SpaceVehicle{
         println("Proceed to hyperSpace")
     }
 }
-class Starship:SpaceVehicle{
-    override fun Accelerating(){
+
+class Starship : SpaceVehicle {
+    override fun Accelerating() {
         println("Luke Iam your father!!")
     }
 
@@ -103,7 +108,93 @@ class Starship:SpaceVehicle{
     }
 }
 
-fun main(){
+interface VehicleProperties {
+    val weight: Int
+    val name: String
+        get() = "Vehicle"
+}
+
+class Car : VehicleProperties {
+    override val weight: Int = 2000
+}
+
+class Tank : VehicleProperties {
+    override val weight: Int
+        get() = 10000
+    override val name: String
+        get() = "Royce"
+}
+
+interface WheeledVehicle : vehicle {
+    val numberOfWheels: Int
+    val WheelSize: Double
+
+}
+
+class Bike : WheeledVehicle {
+    var peddling = false
+    var breaksApplied = false
+    override val numberOfWheels: Int = 2
+    override val WheelSize: Double
+        get() = 40.0
+
+    override fun Accelerating() {
+        var peddling = true
+        var breaksApplied = false
+    }
+
+    override fun Stop() {
+        var peddling = false
+        var breaksApplied = true
+    }
+}
+
+class MotorBike : WheeledVehicle {
+    var speeding = false
+    var drifting = false
+    override val numberOfWheels: Int = 2
+    override val WheelSize: Double
+        get() = 50.0
+
+    override fun Accelerating() {
+        var speeding = true
+        var drifting = false
+    }
+
+    override fun Stop() {
+        var speeding = false
+        var drifting = false
+    }
+}
+
+interface Area {
+    val area: Double
+
+}
+
+fun main() {
+
+    class Square(val side: Double) : Area {
+        override val area: Double
+            get() = side * side
+    }
+
+    class Triangle(val base: Double, val width: Double, val height: Double) : Area {
+        override val area: Double
+            get() = 0.5 * base * width * height
+    }
+
+    class Circle(val radius: Double) : Area {
+        override val area: Double
+            get() = PI * radius * radius
+    }
+
+    val square = Square(5.0)
+    val triangle = Triangle(6.0, 4.0, 3.0)
+    val circle = Circle(4.0)
+    val shape = arrayOf(square, triangle, circle)
+
+    println(shape.map { it.area })
     val StarWars = Starship()
     val richClass = Yatch()
     val MvFaina = cargoShip()
