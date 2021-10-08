9e83f8e79356edb50dc2845ac252fa0ffc3875c4
reptilefury <reptilefury@outlook.com>
2021-10-08 02:27:40 +0100

    Working on generics

--- src/main/kotlin/Chapter 18  Generics/generics.kt
+++ src/main/kotlin/Chapter 18  Generics/generics.kt
@@ -1,6 +1,25 @@
 package `Chapter 18  Generics`
 
 import java.awt.geom.PathIterator
+import java.lang.management.ThreadInfo
+
+fun List<String>.toBulletedList():String{
+    val separator = "\n - "
+    return this.map{"$it"}.joinToString(separator, prefix = separator, postfix = "\n")
+}
+/*fun<T> List<T>.toBulletedList():String{
+    val separator = "\n - "
+    return this.map{"$it"}.joinToString(separator, prefix = separator, postfix = "\n")
+}*/
+
+class Mover<T>(thingsToMove:List<T>,
+val truckHeightInInches:Int = (12 * 12 )
+               ){
+    private var thingsLeftInOldPlace = mutableListOf<T>()
+    private var thingsInTruck = mutableListOf<T>()
+    private var thingsInNewPlace = mutableListOf<T>()
+
+}
 
 fun main(){
     val names = listOf("Ted", "Bob", "Alice", "Lena")
@@ -19,4 +38,12 @@ fun main(){
     )
     //val one = map.get(1)
     val one = map[1]
+   val valuesForKeysWithE = map.keys
+       .filter {it.contains("e")}
+       .map {"Value for $it: ${map[it]}"}
+    println("Values for Keys with E: $valuesForKeysWithE")
+    println("Names: ${names.toBulletedList()}")
+//    println("Names: ${names.toBulletedList()}")
+//println("Things: ${things.toBulletedList()}")
+
  }
