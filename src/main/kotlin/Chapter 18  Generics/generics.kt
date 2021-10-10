a4663740091a711052c6a170b0075e6713d7d8fb
reptilefury <reptilefury@outlook.com>
2021-10-10 03:34:47 +0100

    Ok I'm back full force and working on generics this time

--- src/main/kotlin/Chapter 18  Generics/generics.kt
+++ src/main/kotlin/Chapter 18  Generics/generics.kt
@@ -1,35 +1,73 @@
 package `Chapter 18  Generics`
 
-import java.awt.geom.PathIterator
-import java.lang.management.ThreadInfo
+import javax.swing.border.EmptyBorder
 
-fun List<String>.toBulletedList():String{
+
+fun List<String>.toBulletedList(): String {
     val separator = "\n - "
-    return this.map{"$it"}.joinToString(separator, prefix = separator, postfix = "\n")
+    return this.map { "$it" }.joinToString(separator, prefix = separator, postfix = "\n")
 }
 /*fun<T> List<T>.toBulletedList():String{
     val separator = "\n - "
     return this.map{"$it"}.joinToString(separator, prefix = separator, postfix = "\n")
 }*/
 
-class Mover<T>(thingsToMove:List<T>,
-val truckHeightInInches:Int = (12 * 12 )
-               ){
+
+class Mover<T>(
+    thingstoMove: List<T>,
+    val truckHeightInches: Int = (12 * 12)
+) {
+    // thingsLeftInOldPlace thingsInTruck thingsInNewPlace
     private var thingsLeftInOldPlace = mutableListOf<T>()
-    private var thingsInTruck = mutableListOf<T>()
+    private var thingInTruck = mutableListOf<T>()
     private var thingsInNewPlace = mutableListOf<T>()
+
+    init {
+        thingsLeftInOldPlace.addAll(thingstoMove)
+    }
+    fun moveEverything(){
+        while(thingsLeftInOldPlace.count() > 0){
+           val item = thingsLeftInOldPlace.removeAt(0)
+            thingInTruck.add(item)
+            println("Moved your $item to truck!!")
+        }
+    }
+}
+
+//Kotlin generics explained in full depth
+class Company<T>(text: T) {
+    val x = text
+
+    init {
+        println(x)
+    }
+}
+
+class Event<T, V>(value: T? = null, data: V? = null) {
+    init {
+        if (value != null) {
+            println(value.toString().length)
+        } else println("Empty parameter")
+    }
 
 }
 
-fun main(){
+fun main() {
+    Event<String, Int>("Testing Generics!", 12)
+    Event<Int, String>(256, "Ok so this is 256")
+    Event<Int, String>(12, "ok let's do it again I think I blinked!!")
+    Event<String, Int>()
+    val name: Company<String> = Company<String>("Trying to style this generics!!")
+    val rank: Company<Int> = Company<Int>(12)
+
     val names = listOf("Ted", "Bob", "Alice", "Lena")
     println("Names: $names")
     val firstName = names.first()
 
     println(firstName)
 
-    val things = mutableListOf<Any>(1,2,3)
-      things.add("Steve")
+    val things = mutableListOf<Any>(1, 2, 3)
+    things.add("Steve")
     println("things: $things")
     val map = mapOf(
         Pair("one", 1 ),
