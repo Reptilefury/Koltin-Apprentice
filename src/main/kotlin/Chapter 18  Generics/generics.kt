d53586a9753764502ff7e3cb40e3cf0c567efdc0
reptilefury <reptilefury@outlook.com>
2021-10-11 02:39:45 +0100

    Still doing generics

--- src/main/kotlin/Chapter 18  Generics/generics.kt
+++ src/main/kotlin/Chapter 18  Generics/generics.kt
@@ -25,12 +25,25 @@ class Mover<T>(
     init {
         thingsLeftInOldPlace.addAll(thingstoMove)
     }
-    fun moveEverything(){
-        while(thingsLeftInOldPlace.count() > 0){
-           val item = thingsLeftInOldPlace.removeAt(0)
+
+    fun moveEverything() {
+        while (thingsLeftInOldPlace.count() > 0) {
+            val item = thingsLeftInOldPlace.removeAt(0)
             thingInTruck.add(item)
             println("Moved your $item to truck!!")
         }
+    }
+
+    fun moveEverythingIntoNewPlace() {
+        while (thingInTruck.count() > 0) {
+            val item = thingInTruck.removeAt(0)
+            thingsInNewPlace.add(item)
+            print("Your $item was moved into your new place!!")
+        }
+    }
+
+    fun finishMove() {
+        println("Ok we are done! we were able to move your ${"Bullted list function missing"} ")
     }
 }
 
@@ -52,7 +65,23 @@ class Event<T, V>(value: T? = null, data: V? = null) {
 
 }
 
+class CheapThing(val name: String) {
+    override fun toString(): String {
+        return name
+    }
+}
 fun main() {
+    val cheapThings = listOf(
+        CheapThing("Coffee Table"),
+        CheapThing("BeanBag chair"),
+        CheapThing("Study table"),
+        CheapThing("Carpet"),
+        )
+
+  val  CheapMover = Mover(cheapThings)
+    CheapMover.moveEverything()
+    CheapMover.moveEverythingIntoNewPlace()
+    CheapMover.finishMove()
     Event<String, Int>("Testing Generics!", 12)
     Event<Int, String>(256, "Ok so this is 256")
     Event<Int, String>(12, "ok let's do it again I think I blinked!!")
