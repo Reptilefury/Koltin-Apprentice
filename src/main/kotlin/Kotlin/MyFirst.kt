d45e09b515c4ba54b1dbf5e3985c3d66a87e66d7
reptilefury <reptilefury@outlook.com>
2021-10-27 09:03:03 +0100

    Still working on Kotlin Java InterOp

---
+++ src/main/kotlin/Kotlin/MyFirst.kt
@@ -0,0 +1,7 @@
+package Kotlin
+
+class MyFirst {
+    fun add(a: Int, b: Int):Int {
+        return a + b
+    }
+}
---
+++ src/main/kotlin/Kotlin/MyJavaFile.java
@@ -0,0 +1,10 @@
+package Kotlin;
+
+public class MyJavaFile {
+    public static void main(String[] args) {
+
+        int sum = MyFirst.add(2, 4);
+
+
+    }
+}
