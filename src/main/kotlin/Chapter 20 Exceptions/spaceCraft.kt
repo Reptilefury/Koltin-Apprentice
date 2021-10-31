0baba6b0b7087169d7ab684c05bee01edaf2c00a
reptilefury <reptilefury@outlook.com>
2021-10-31 02:24:21 +0000

    Will start on creating custom exceptions

--- src/main/kotlin/Chapter 20 Exceptions/spaceCraft.kt
+++ src/main/kotlin/Chapter 20 Exceptions/spaceCraft.kt
@@ -1,37 +1,53 @@
 package `Chapter 20 Exceptions`
 
-
-//isConnectionAvailable= no connection with earth cannot take off, isEngineInOrder= the engine is broken cannot take off, isInSpace = true, fuel = 0
-fun main(){
-
-    class spaceCraft {
-        var isConnectionAvailable:Boolean = false
-        var isEngineInOrder:Boolean = false
-        var isInSpace:Boolean = false
-        var fuel:Int = 0
-        fun launch(){
-            if(fuel<5){
-                sendMessageToEarth("Out of fuel cannot take off")
-                return
-            }
-            if(isConnectionAvailable){
-                sendMessageToEarth("no connection to Earth Cannot take off")
-                return
-            }
-            if(isEngineInOrder){
-                sendMessageToEarth("the engine is broken cannot take off")
-                return
-            }
-            sendMessageToEarth("Trying to launch so chill TF Out ")
-            fuel -= 5
-            sendMessageToEarth("I'm in Space, chillin with some aliens man!! say cheese Mr dOOBIE DUB!")
-            sendMessageToEarth("Yoo I got some UFOs mfs (Unidentified flying Objects")
-            isInSpace = true
-
+object SpacePort {
+    fun investigateSpace(spaceCraft: SpaceCraft) {
+        try {
+            spaceCraft.launch()
+        } catch (exception: Exception) {
+            spaceCraft.sendMessageToEarth(exception.localizedMessage)
         }
-        fun sendMessageToEarth(message:String){
-            println("SpaceCraftToEarth:$message")
-        }
-
     }
 }
+
+class spaceCraft {
+    var isConnectionAvailable: Boolean = false
+    var isEngineInOrder: Boolean = false
+    var isInSpace: Boolean = false
+    var fuel: Int = 0
+    fun launch() {
+        if (fuel < 5) {
+            throw Exception("Out of fuel cannot take off")
+            return
+        }
+        if (isConnectionAvailable) {
+            throw Exception("No connection to earth cannot take off!!")
+            return
+        }
+        if (isEngineInOrder) {
+            throw Exception("Engine is out of fuel cannot take off!!")
+            return
+        }
+        throw Exception("I'm trynna launch man so chill tf out")
+        fuel -= 5
+        throw Exception("I'm in space man chilling with some fucking aliens man, say: Cheese Mr Doobie Duh!!")
+        throw Exception("Yoo I got some UFO's (Unidentified Flying Objects")
+        isInSpace = true
+
+    }
+
+    fun sendMessageToEarth(message: String) {
+        println("SpaceCraftToEarth:$message")
+    }
+
+
+}
+
+//isConnectionAvailable= no connection with earth cannot take off, isEngineInOrder= the engine is broken cannot take off, isInSpace = true, fuel = 0
+fun main() {
+    val SpaceCraft = spaceCraft()
+    SpacePort.investigateSpace(SpaceCraft())
+
+
+}
+//Will start on creating custom exceptions
