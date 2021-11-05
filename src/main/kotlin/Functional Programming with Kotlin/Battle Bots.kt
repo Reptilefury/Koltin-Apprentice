b876d5fcd5e9a6f5827bf5d51884ad30f4fe84c2
reptilefury <reptilefury@outlook.com>
2021-11-05 02:20:56 +0000

    I'm currently working on Functinal programming with kotlin

--- src/main/kotlin/Functional Programming with Kotlin/Battle Bots.kt
+++ src/main/kotlin/Functional Programming with Kotlin/Battle Bots.kt
@@ -1,12 +1,37 @@
 package `Functional Programming with Kotlin`
 import java.util.*
 
-import sun.security.util.Event.report
 import kotlin.random.Random
 
 
 class Robot(val name: String){
     var strength: Int = 0
+    var isAlive:Boolean = true
+
+     fun attack(robot:Robot){
+
+         val damage = (strength * 0.1 + Random.nextInt(10).toInt() )
+
+         robot.damage(damage.toInt())
+
+     }
+
+    fun damage(damage:Int){
+        val blocked = Random.nextBoolean()
+        if(blocked){
+            report("Blocked attack")
+            return
+        }
+        health -= damage
+       report("Damage -$damage , health $health")
+
+        if(health <= 0){
+            isAlive = false
+        }
+    }
+
+
+
     private var health:Int = 0
 
     init {
@@ -17,6 +42,29 @@ class Robot(val name: String){
     fun report(message:String){
         println("$name: \t$message")
     }
+    object BattleField{
+        fun BeginBattle(firstRobot: Robot, secondRobot:Robot){
+            var winner:Robot? = null
+            battle(firstRobot, secondRobot)
+            winner = if(firstRobot.isAlive) firstRobot else secondRobot
+        }
+        fun battle(firstRobot:Robot, secondRobot:Robot){
+            firstRobot.attack(secondRobot)
+            if(secondRobot.isAlive.not()){
+                return
+            }
+            secondRobot.attack(firstRobot)
+            if(firstRobot.isAlive.not()){
+                return
+            }
+            battle(firstRobot, secondRobot)
+        }
+    }
+}
+fun main() {
+   val firstRobot = Robot("Experimental Space Navigation")
+   val secondRobot = Robot("Extra-terrestrial safety droid")
+    Robot.BattleField.BeginBattle(firstRobot, secondRobot)
 }
 /*
 class Robot(val name : String){
