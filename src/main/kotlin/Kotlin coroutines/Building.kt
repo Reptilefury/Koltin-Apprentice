8b1579ce2044b1f08f0f53d9285fbae8c0586925
reptilefury <reptilefury@outlook.com>
2021-11-17 02:43:47 +0000

    Still working on Kotlin Coroutines

--- src/main/kotlin/Kotlin coroutines/Building.kt
+++ src/main/kotlin/Kotlin coroutines/Building.kt
@@ -2,36 +2,53 @@ package `Kotlin coroutines`
 
 class  Building(val name:String){
 
-    fun makeFoundation(function: () -> Unit) {
-        Thread.sleep(300)
-        speakThroughBullHorn("Foundation is ready, get ready to rumble!!")
-    }
-    fun buildFloor(floor:Int){
-        Thread.sleep(100)
-        speakThroughBullHorn("$floor'th floor is raised")
-    }
-    fun placeWindows(floor:Int){
-        Thread.sleep(100)
-        speakThroughBullHorn("Windows are places on the $floor'th floor")
-    }
-    fun installDoors(floor:Int){
-        Thread.sleep(100)
-        speakThroughBullHorn("Doors are installed on the $floor'th floor ")
-    }
-    fun provideElectricity(floor:Int){
-        Thread.sleep(100)
-        speakThroughBullHorn("Electricity is provided on the $floor'th floor")
-    }
-    fun buildRoof(floor:Int){
-        Thread.sleep(200)
-        speakThroughBullHorn("The roof is ready ")
-    }
-    fun fitOut(floor:Int){
-        Thread.sleep(200)
-        speakThroughBullHorn("The $floor'th is furnished")
+    suspend fun makeFoundation() = launch {
+        delay(300)
+        //Thread.sleep(300)
+        speakThroughBullHorn("[${Thread.currentThread().name}] The foundation is ready, get ready to rumble!!")
     }
 
-    fun speakThroughBullHorn(message:String)= println(message)
+    suspend fun buildFloor(floor: Int) = launch {
+        delay(100)
+        //Thread.sleep(100)
+        //speakThroughBullHorn("$floor'th floor is raised")
+        speakThroughBullHorn("[${Thread.currentThread().name}] FLoor number  $floor is built")
+        ++floors
+    }
+
+    suspend fun placeWindows(floor: Int) = launch {
+        delay(100)
+        //Thread.sleep(100)
+        speakThroughBullHorn("[${Thread.currentThread().name}] Windows are places on the floor number $floor")
+    }
+
+    suspend fun installDoors(floor: Int) = launch {
+        delay(100)
+        //Thread.sleep(100)
+        speakThroughBullHorn("[${Thread.currentThread().name}] Doors are installed on the floor number $floor")
+    }
+
+    suspend fun provideElectricity(floor: Int) = launch {
+        delay(100)
+        //Thread.sleep(100)
+        speakThroughBullHorn("[${Thread.currentThread().name}] Electricity is provided on the floor number $floor")
+    }
+
+    suspend fun buildRoof(floor: Int) = launch {
+        delay(200)
+        // Thread.sleep(200)
+        speakThroughBullHorn("[${Thread.currentThread().name}] The roof is ready")
+//speakThroughBullHorn("The roof is ready ")
+    }
+
+  suspend fun fitOut(floo Int) = launch {
+        delay(200)
+        //Thread.sleep(200)
+    speakThroughBullHorn("[$Thread.currentThread().name]Floor number is $floor is furnished")
+    // speakThroughBullHorn("The $floor'th is furnished")
+    }
+
+    fun speakThroughBullHorn(message: String) = println(message)
 
 
 }
--- src/main/kotlin/Kotlin coroutines/Main.kt
+++ src/main/kotlin/Kotlin coroutines/Main.kt
@@ -2,4 +2,21 @@ package `Kotlin coroutines`
 
 fun main(){
     BuildingYard.startProject("SmartHouse", 20)
+    async {
+        (0..10).forEach{
+            println("Message #$it from the ${Thread.currentThread().name} ")
+        }
+
+
+    }
+    (0..10).forEach{
+        println("Message #$it from the${Thread.currentThread().name}")
+    }
+
 }
+public actual fun launch(
+    context: CoroutineContext =  DefaultDispatcher,
+    start:CoroutineStart = CoroutineStart.DEFAULT,
+    parent:Job?= null,
+    block: suspend CoroutineScope.() -> Unit
+):Job
